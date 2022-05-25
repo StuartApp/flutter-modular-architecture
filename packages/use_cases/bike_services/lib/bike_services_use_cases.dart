397629463c99defea7d58a298d01fb2bb344f636
Pau Picas <p.picas@stuart.com>
2022-05-25 23:00:59 +0200

    Export `SortedServicesResponse` from `bike_services` use cases module

--- packages/use_cases/bike_services/lib/bike_services_use_cases.dart
+++ packages/use_cases/bike_services/lib/bike_services_use_cases.dart
@@ -2,6 +2,10 @@
 export 'src/models/city.dart' show City;
 export 'src/models/service.dart' show Service;
 
+// Responses
+export 'src/responses/sorted_services_response.dart'
+    show SortedServicesResponse;
+
 // Use cases
 export 'src/use_cases/get_all_services.dart' show GetAllServices;
 export 'src/use_cases/get_all_services_sorted_by_distance.dart'
